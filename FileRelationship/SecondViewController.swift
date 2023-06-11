00e1bc8784cae566363de07e1e225dccf26143e3
taiga07 <taigatani@me.com>
2023-06-11 11:31:35 +0900

    first commit

---
+++ FileRelationship/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  FileRelationship
+//
+//  Created by 谷太海 on 2023/06/11.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var label2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label2.text = "２つ目の画面"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FileRelationship/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  FileRelationship
+//
+//  Created by 谷太海 on 2023/06/11.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    @IBOutlet weak var label3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label3.text = "3つ目の画面"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
